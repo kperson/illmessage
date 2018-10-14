89d2afed8e27be2b2fc933843a921be1959fcb30
Kelton Person <kelton.person@gmail.com>
2018-10-14 16:17:17 -0500

    added API gatwway permissions

--- build/terraform/api.tf
+++ build/terraform/api.tf
@@ -9,7 +9,7 @@ resource "aws_api_gateway_rest_api" "api" {
 resource "aws_api_gateway_resource" "api" {
   rest_api_id = "${aws_api_gateway_rest_api.api.id}"
   parent_id   = "${aws_api_gateway_rest_api.api.root_resource_id}"
-  path_part   = "$${proxy+}"
+  path_part   = "{proxy+}"
 }
 
 resource "aws_api_gateway_method" "api" {
