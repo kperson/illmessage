c6be470adfa375b674b007ab158b05c68de1a40e
Kelton Person <kelton.person@gmail.com>
2018-10-14 16:24:32 -0500

    added POST integration

--- build/terraform/api.tf
+++ build/terraform/api.tf
@@ -20,11 +20,12 @@ resource "aws_api_gateway_method" "api" {
 }
 
 resource "aws_api_gateway_integration" "api" {
-  rest_api_id = "${aws_api_gateway_rest_api.api.id}"
-  resource_id = "${aws_api_gateway_resource.api.id}"
-  http_method = "${aws_api_gateway_method.api.http_method}"
-  type        = "AWS_PROXY"
-  uri         = "arn:aws:apigateway:${var.region}:lambda:path/2015-03-31/functions/${aws_lambda_function.api.arn}/invocations"
+  rest_api_id             = "${aws_api_gateway_rest_api.api.id}"
+  resource_id             = "${aws_api_gateway_resource.api.id}"
+  http_method             = "${aws_api_gateway_method.api.http_method}"
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = "arn:aws:apigateway:${var.region}:lambda:path/2015-03-31/functions/${aws_lambda_function.api.arn}/invocations"
 }
 
 resource "aws_lambda_permission" "api" {
