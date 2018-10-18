5ca3c41d91e8d7d2d4fd26599af764d50204c374
Kelton Person <kelton.person@gmail.com>
2018-10-17 20:58:12 -0500

    added more logging

--- app/src/main/scala/com/github/kperson/app/MessageProcessor.scala
+++ app/src/main/scala/com/github/kperson/app/MessageProcessor.scala
@@ -1,12 +1,20 @@
 package com.github.kperson.app
 
-import com.github.kperson.aws.dynamo.{ChangeCapture, DynamoMap, StreamChangeCaptureHandler}
+import com.github.kperson.aws.dynamo.{ChangeCapture, DynamoMap, New, StreamChangeCaptureHandler}
+import org.slf4j.LoggerFactory
 
 class MessageProcessor extends StreamChangeCaptureHandler {
 
+  val logger = LoggerFactory.getLogger(getClass)
+
   def handleChange(change: ChangeCapture[DynamoMap]) {
-    println("change")
-    println(change)
+    logger.debug(s"processing change, $change")
+    change.map { _.flatten } match {
+      case New(_, item) =>
+        logger.info(s"received new payload, $item")
+        println(item)
+      case _ =>
+    }
   }
 
 }
