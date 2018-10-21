56683ff0f2d5aa37d4085071f7d82782ab6e72d8
Kelton Person <kelton.person@gmail.com>
2018-10-21 18:09:49 -0500

    added policy document

--- build/terraform/task-build.tf
+++ build/terraform/task-build.tf
@@ -28,5 +28,68 @@ resource "aws_ecs_task_definition" "background" {
   network_mode             = "awsvpc"
   requires_compatibilities = ["EC2", "FARGATE"]
   task_role_arn            = "${aws_iam_role.tasks_role.arn}"
+  execution_role_arn       = "${aws_iam_role.execution.arn}"
   container_definitions    = "${data.template_file.api_container_definitions.rendered}"
+}
+
+data "aws_iam_policy_document" "execution_role_policy" {
+  statement {
+    actions = [
+      "ecr:GetAuthorizationToken",
+    ]
+
+    resources = [
+      "*",
+    ]
+  }
+
+  statement {
+    actions = [
+      "ecr:BatchCheckLayerAvailability",
+      "ecr:GetDownloadUrlForLayer",
+      "ecr:BatchGetImage",
+    ]
+
+    resources = [
+      "${aws_ecr_repository.repo.arn}",
+    ]
+  }
+
+  statement {
+    actions = [
+      "logs:CreateLogStream",
+      "logs:PutLogEvents",
+    ]
+
+    resources = [
+      "arn:aws:logs:${var.region}:${data.aws_caller_identity.current.account_id}:log-group:${var.namespace}",
+      "arn:aws:logs:${var.region}:${data.aws_caller_identity.current.account_id}:log-group:${var.namespace}:*",
+    ]
+  }
+}
+
+data "aws_iam_policy_document" "execution_assume_role_policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["ecs-tasks.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_policy" "execution" {
+  name   = "execution_policy"
+  policy = "${data.aws_iam_policy_document.execution_role_policy.json}"
+}
+
+resource "aws_iam_role" "execution" {
+  name               = "execution"
+  assume_role_policy = "${data.aws_iam_policy_document.execution_assume_role_policy.json}"
+}
+
+resource "aws_iam_role_policy_attachment" "execution_policy" {
+  role       = "${aws_iam_role.rexecution.name}"
+  policy_arn = "${aws_iam_policy.execution.arn}"
 }
