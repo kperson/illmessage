69e0f4cc7db85ed1d1780899f465335bbd698a96
Kelton Person <kelton.person@gmail.com>
2018-10-21 22:30:29 -0500

    trying more logging

--- app/src/main/scala/com/github/kperson/api/DeadLetterAPI.scala
+++ app/src/main/scala/com/github/kperson/api/DeadLetterAPI.scala
@@ -3,14 +3,18 @@ package com.github.kperson.api
 import akka.http.scaladsl.model._
 import akka.http.scaladsl.server
 import akka.http.scaladsl.server.Directives._
-
 import com.github.kperson.deadletter.DeadLetterQueueDAO
 import com.github.kperson.model.MessageSubscription
+import org.slf4j.LoggerFactory
+
+import scala.util.{Failure, Success}
 
 
 trait DeadLetterAPI extends MarshallingSupport {
 
   def deadLetterQueueDAO: DeadLetterQueueDAO
+
+  private val logger = LoggerFactory.getLogger(getClass)
 
   val deadLetterRoute: server.Route = {
     pathPrefix("dead-letter") {
@@ -21,8 +25,11 @@ trait DeadLetterAPI extends MarshallingSupport {
               val subscription = MessageSubscription(exchange, bindingKey, queue, accountId)
               path("redeliver") {
                 post {
-                  onSuccess(deadLetterQueueDAO.triggerRedeliver(subscription)) { _ =>
-                    complete(StatusCodes.NoContent)
+                  onComplete(deadLetterQueueDAO.triggerRedeliver(subscription)) {
+                    case Success(_) => complete(StatusCodes.NoContent)
+                    case Failure(err) =>
+                      logger.error("redeliver enqueue failed", err)
+                      complete(StatusCodes.InternalServerError)
                   }
                 }
               }
