b2efe071045359419956604701bff74b30046c7c
Kelton Person <keltonperson@Keltons-MacBook-Pro.local>
2019-06-08 08:37:01 -0500

    made loop sync

--- app/src/main/scala/com/github/kperson/api/Main.scala
+++ app/src/main/scala/com/github/kperson/api/Main.scala
@@ -2,7 +2,6 @@ package com.github.kperson.api
 
 import com.amazonaws.services.lambda.runtime.RequestStreamHandler
 import com.github.kperson.aws.{AWSHttp, AWSHttpResponse}
-
 import java.io.{ByteArrayInputStream, ByteArrayOutputStream, PrintWriter, StringWriter}
 import java.net.URI
 import java.net.http.HttpRequest.BodyPublishers
@@ -13,9 +12,8 @@ import org.json4s.jackson.Serialization._
 import org.json4s.NoTypeHints
 import org.json4s.jackson.Serialization
 
-import scala.concurrent.ExecutionContext.Implicits.global
-import scala.concurrent.Future
-
+import scala.concurrent.{Await, Future}
+import scala.concurrent.duration._
 import AWSHttp._
 
 
@@ -37,43 +35,44 @@ object  Main {
 
   def runRequest(method: String, url: String, body: Array[Byte] = Array.emptyByteArray): Future[AWSHttpResponse] = {
     val builder = HttpRequest.newBuilder(new URI(url))
-    builder.method(method, BodyPublishers.ofByteArray(body))
+    builder.method(method, BodyPublishers.ofByteArray(body)).timeout(java.time.Duration.ofSeconds(61))
     client.future(builder.build())
   }
 
   def run(runtimeApiEndpoint: String, handler: RequestStreamHandler) {
-    runRequest("GET", s"http://abc/2018-06-01/runtime/invocation/next")
-      .foreach { rs =>
-      val requestId = rs.lowerHeaders("Lambda-Runtime-Aws-Request-Id".toLowerCase)
-      try {
-        val out = new ByteArrayOutputStream()
-        handler.handleRequest(new ByteArrayInputStream(rs.body), out, null)
-        runRequest(
-          "POST",
-          s"http://$runtimeApiEndpoint/2018-06-01/runtime/invocation/$requestId/response",
-          out.toByteArray
-        ).foreach { _ =>
-          run(runtimeApiEndpoint, handler)
-        }
-      }
-      catch {
-        case ex: Throwable =>
-          val sw = new StringWriter()
-          val pw = new PrintWriter(sw)
-          val errorPayload = Map(
-            "errorMessage" -> ex.getMessage,
-            "localizedMessage" -> ex.getMessage,
-            "stackTrace" -> ex.printStackTrace(pw)
-          )
-          val errorMessage = write(errorPayload)(Serialization.formats(NoTypeHints))
+    val jobFetchFuture = runRequest("GET", s"http://$runtimeApiEndpoint/2018-06-01/runtime/invocation/next")
+    val jobFetch = Await.result(jobFetchFuture, 30.seconds)
+    val requestId = jobFetch.lowerHeaders("Lambda-Runtime-Aws-Request-Id".toLowerCase)
+    try {
+      val out = new ByteArrayOutputStream()
+      handler.handleRequest(new ByteArrayInputStream(jobFetch.body), out, null)
+      Await.result(runRequest(
+        "POST",
+        s"http://$runtimeApiEndpoint/2018-06-01/runtime/invocation/$requestId/response",
+        out.toByteArray
+      ), 60.seconds)
+      run(runtimeApiEndpoint, handler)
+    }
+    catch {
+      case ex: Throwable =>
+        val sw = new StringWriter()
+        val pw = new PrintWriter(sw)
+        val errorPayload = Map(
+          "errorMessage" -> ex.getMessage,
+          "localizedMessage" -> ex.getMessage,
+          "stackTrace" -> ex.printStackTrace(pw)
+        )
+        val errorMessage = write(errorPayload)(Serialization.formats(NoTypeHints))
+        System.err.println(errorMessage)
+        Await.result(
           runRequest(
             "POST",
             s"http://$runtimeApiEndpoint/2018-06-01/runtime/invocation/$requestId/error",
             errorMessage.getBytes(StandardCharsets.UTF_8)
-          ).foreach { _ =>
-            run(runtimeApiEndpoint, handler)
-          }
-      }
+          ),
+          60.seconds
+        )
+        run(runtimeApiEndpoint, handler)
     }
   }
 
