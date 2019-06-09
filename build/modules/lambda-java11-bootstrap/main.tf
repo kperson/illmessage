9c4d87280437d02131b1476506c31e72a64a5bce
Kelton Person <keltonperson@Keltons-MacBook-Pro.local>
2019-06-09 09:31:35 -0500

    added ls command

--- build/modules/lambda-java11-bootstrap/main.tf
+++ build/modules/lambda-java11-bootstrap/main.tf
@@ -27,7 +27,7 @@ resource "null_resource" "zip" {
   }
 
   provisioner "local-exec" {
-    command = "chmod +x bootstrap && zip ${random_string.tag.result}.zip bootstrap ${var.jar_file} && while [ ! -f ${random_string.tag.result}.zip ]; do sleep 1; done"
+    command = "chmod +x bootstrap && zip ${random_string.tag.result}.zip bootstrap ${var.jar_file} && ls && while [ ! -f ${random_string.tag.result}.zip ]; do sleep 1; done"
   }
 }
 
